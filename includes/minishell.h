5db79be34bb8c42eab36157a8c577a28ac81e1fc
Xavier-LB <xavier.lebaux@gmail.com>
2022-05-18 10:39:49 +0200

    add structs for tokens

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wdebotte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: xlb <xlb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:53:23 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/05/17 12:58:44 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/05/18 10:39:23 by xlb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+
+enum	e_type_token
+{
+	WORD = 1,
+	QUOTE,
+	D_QUOTE,
+	NLINE,
+	WHITE_SPACE,
+	GREAT,
+	D_GREAT,
+	LESS,
+	D_LESS,
+	PIPE,
+	DOLLAR,
+	BACKSLASH
+};
+
+typedef struct s_token
+{
+	struct s_tok	        *next;
+	int			type;
+	char			*content;
+}				t_token;
 
 #endif
