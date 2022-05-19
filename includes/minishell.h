9545ac7c60265ebe4472908c3bba3b5346f4f92b
William DEBOTTE <wdebotte@student.42.fr>
2022-05-19 10:14:29 +0200

    Main modifications

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,13 +6,14 @@
 /*   By: wdebotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:53:23 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/05/17 13:22:49 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/05/19 09:49:19 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,25 @@
 /*   By: wdebotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:54:03 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/05/17 13:26:05 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/05/19 10:10:57 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+We can use readline() function but it does a lot of leaks
+char *readline(const char *prompt);
+*/
 int	main(void)
 {
 	char	*line;
 
 	while (1)
 	{
-		line = readline("$> ");
-		printf("%s\n", line);
+		_putstr(">$ ");
+		line = get_next_line(0);
+		printf("%s", line);
 		free(line);
 		line = NULL;
 	}
