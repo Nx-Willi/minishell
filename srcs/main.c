a191732c9b776b9498966c5ed231b17e08a96c75
William DEBOTTE <wdebotte@student.42.fr>
2022-05-19 13:31:51 +0200

    Save change cluster

--- srcs/main.c
+++ srcs/main.c
@@ -6,27 +6,20 @@
 /*   By: wdebotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:54:03 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/05/19 10:57:46 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/05/19 12:55:59 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
-We can use readline() function but it does a lot of leaks
-char *readline(const char *prompt);
-*/
 int	main(void)
 {
 	char	*line;
 
 	while (1)
 	{
-		ft_putstr(">$ ");
-		line = get_next_line(0);
-		printf("%s", line);
-		if (is_builtin(line))
-			printf("BUILTIN !\n");
+		line = readline(">$ ");
+		printf("%s\n", line);
 		free(line);
 		line = NULL;
 	}
