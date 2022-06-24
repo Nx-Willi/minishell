a33d45c1470868db8878b0f1a854d90c5513dbc9
William DEBOTTE <wdebotte@student.42.fr>
2022-06-24 14:58:41 +0200

    Add echo builtin

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,13 +6,47 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 12:37:38 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/06/24 12:38:20 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/06/24 14:45:16 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	check_param(char *param)
+{
+	int	i;
+
+	i = 1;
+	while (param[i] != '\0')
+		if (param[i++] != 'n')
+			return (FALSE);
+	return (TRUE);
+}
+
 void	builtin_echo(t_cmd *cmd)
 {
-	
+	int	i;
+	int	newline;
+
+	newline = TRUE;
+	i = 0;
+	while (cmd->argv[++i] != NULL)
+	{
+		if (cmd->argv[i][0] == '-')
+		{
+			if (check_param(cmd->argv[i]) == TRUE)
+				newline = FALSE;
+			else
+				break ;
+		}
+		else
+			break ;
+	}
+	while (cmd->argv[i] != NULL)
+	{
+		ft_putstr(cmd->argv[i++]);
+		ft_putchar(' ');
+	}
+	if (newline == TRUE)
+		ft_putchar('\n');
 }
