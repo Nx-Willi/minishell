4e0edfd1c46f0ae8be1f72fd488dd5da98d0c92e
William <william.debotte77@gmail.com>
2022-06-30 19:08:43 +0200

    Remove merge control conflict

--- srcs/parser/dollar_format.c
+++ srcs/parser/dollar_format.c
@@ -6,7 +6,7 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 14:59:57 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/06/30 18:35:49 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/06/30 19:07:45 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,9 @@ void	dollar_format(t_infos *infos, t_token *token)
 			free (token->content);
 			if (get_env_var_value(infos, token->next->content) != NULL)
 			{
-<<<<<<< HEAD
-			token->content = ft_strdup(get_env_var_value(infos,
-							token->next->content));
-			token->type = WORD;
-=======
 				token->content = ft_strdup(get_env_var_value(infos,
 							token->next->content));
 				token->type = WORD;
->>>>>>> wdebotte
 			}
 			else
 			{
