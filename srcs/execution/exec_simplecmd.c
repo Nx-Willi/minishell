4716dc45a88d9e9e12cf75c391014a44569c6f38
William DEBOTTE <wdebotte@student.42.fr>
2022-07-11 17:22:50 +0200

    Redirection for Right and Double right: OK

--- srcs/execution/exec_simplecmd.c
+++ srcs/execution/exec_simplecmd.c
@@ -6,7 +6,7 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:49:58 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/06/30 18:55:55 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/07/11 14:30:17 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	exec_simple(t_cmd *cmd)
 	int	pid;
 	int	status;
 
+	if (cmd->fd_out != STDOUT_FILENO)
+		cmd->fd_tmp = dup(STDOUT_FILENO);
 	pid = fork();
 	if (pid < 0)
 	{
@@ -25,9 +27,15 @@ void	exec_simple(t_cmd *cmd)
 	}
 	else if (pid == 0)
 	{
+		if (cmd->fd_out != STDOUT_FILENO)
+		{
+			close(STDOUT_FILENO);
+			dup2(cmd->fd_out, STDOUT_FILENO);
+		}
 		exec_cmd(cmd);
-		exit(EXIT_FAILURE);
 	}
+	if (cmd->fd_out != STDOUT_FILENO)
+		dup2(cmd->fd_tmp, STDOUT_FILENO);
 	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
 		cmd->infos->wstatus = WEXITSTATUS(status);
