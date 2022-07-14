af342e8594f341dc1fdc5361d51d58b4b98c3bb3
XavierLB <xavier.lebaux@gmail.com>
2022-07-14 09:15:36 +0200

    exit_status

--- srcs/parser/dollar_format.c
+++ srcs/parser/dollar_format.c
@@ -6,37 +6,60 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 14:59:57 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/07/11 11:32:33 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/07/14 09:13:22 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	dollar_format(t_infos *infos, t_token *token)
+int	g_exit_status;
+
+static t_token	*exit_status_check(t_token *token)
 {
 	t_token	*tmp;
 
-	while (token->next)
+	token->type = WORD;
+	tmp = token->next->next;
+	free(token->next->content);
+	free(token->next);
+	token->next = tmp;
+	token->content[0] = g_exit_status + '0';
+	return (token);
+}
+
+static t_token	*get_env(t_infos *infos, t_token *token)
+{
+	t_token	*tmp;
+
+	tmp = token->next->next;
+	free (token->content);
+	if (get_env_var_value(infos, token->next->content) != NULL)
 	{
-		if (token->type == DOLLAR)
-		{
-			tmp = token->next->next;
-			free (token->content);
-			if (get_env_var_value(infos, token->next->content) != NULL)
-			{
-				token->content = ft_strdup(get_env_var_value(infos,
-							token->next->content));
-				token->type = WORD;
-			}
-			else
-			{
-				token->content = NULL;
-				token->type = 0;
-			}
-			free(token->next->content);
-			free(token->next);
-			token->next = tmp;
-		}
+		token->content = ft_strdup(get_env_var_value(infos,
+					token->next->content));
+		token->type = WORD;
+	}
+	else
+	{
+		token->content = NULL;
+		token->type = 0;
+	}
+	free(token->next->content);
+	free(token->next);
+	token->next = tmp;
+}
+
+void	dollar_format(t_infos *infos, t_token *token)
+{
+	while (token != NULL)
+	{
+		if (token->type == DOLLAR && (token->next->type == 0
+				|| token->next->type == WHITE_SPACE))
+			token->type = WORD;
+		else if (token->type == DOLLAR && _strcmp(token->next->content, "?"))
+			token = exit_status_check(token);
+		else if (token->type == DOLLAR)
+			token = get_env(infos, token);
 		token = token->next;
 	}
 }
