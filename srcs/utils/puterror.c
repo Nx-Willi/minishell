c0c1fe45706f2f90ff3adc5b720fccc486a65f78
William DEBOTTE <wdebotte@student.42.fr>
2022-07-21 16:00:25 +0200

    Check if argv[0] exist before printing error

--- srcs/utils/puterror.c
+++ srcs/utils/puterror.c
@@ -6,7 +6,7 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 12:40:56 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/07/21 12:55:37 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/07/21 15:59:10 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	puterror(t_cmd *cmd, char *arg, char *error)
 {
+	if (cmd->argv[0][0] != '\0')
+		return ;
 	ft_putstr_fd(SH_NAME": ", 2);
 	ft_putstr_fd(cmd->argv[0], 2);
 	ft_putstr_fd(": ", 2);
