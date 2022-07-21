82a9b441c6ccd243fac8a0f1f9e71edfdb158859
William DEBOTTE <wdebotte@student.42.fr>
2022-07-21 14:16:46 +0200

    Support SHLVL variable

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,36 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 12:54:03 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/07/20 16:49:44 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/07/21 14:15:39 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 int	g_exit_status = 0;
+
+static void	set_shlvl_var(t_infos *infos)
+{
+	int		old_shlvl;
+	char	*new_shlvl;
+	char	*new_var;
+	char	buffer[7];
+
+	if (is_var_in_env(infos->env, "SHLVL") == FALSE)
+	{
+		add_env_var(infos, "SHLVL=1", FALSE);
+		return ;
+	}
+	old_shlvl = ft_atoi(get_env_var_value(infos, "SHLVL"));
+	old_shlvl++;
+	new_shlvl = ft_itoa(old_shlvl);
+	buffer[0] = '\0';
+	ft_strlcat(buffer, "SHLVL=", 7);
+	new_var = ft_strjoin(buffer, new_shlvl);
+	free(new_shlvl);
+	add_env_var(infos, new_var, FALSE);
+	free(new_var);
+}
 
 static void	init_main(t_infos *infos, int argc, char **argv, char **envp)
 {
@@ -27,6 +50,8 @@ static void	init_main(t_infos *infos, int argc, char **argv, char **envp)
 	cpy_env_to_char(infos);
 	if (is_var_in_env(infos->env, "PWD") == FALSE)
 		set_pwd_var(infos);
+	set_shlvl_var(infos);
+	cpy_env_to_char(infos);
 }
 
 static int	get_line_infos(t_infos *infos)
