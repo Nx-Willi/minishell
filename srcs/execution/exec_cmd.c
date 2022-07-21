544b05fe3e97186bdf527951bcd4533b3a0b16c9
William DEBOTTE <wdebotte@student.42.fr>
2022-07-21 18:52:12 +0200

    Fix first fd not closed when exit from child

--- srcs/execution/exec_cmd.c
+++ srcs/execution/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 15:44:38 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/07/21 15:30:07 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/07/21 18:48:30 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,12 @@ void	exec_cmd(t_cmd *cmd)
 	if (access(cmd->cmd_path, F_OK) == -1)
 	{
 		puterror(cmd, NULL, "Command not found");
-		exit(DOESNT_EXIST);
+		exit_program(cmd->infos, DOESNT_EXIST);
 	}
 	else if (access(cmd->cmd_path, X_OK) == -1)
 	{
 		puterror(cmd, NULL, "Permission denied");
-		exit(BAD_PERMIT);
+		exit_program(cmd->infos, BAD_PERMIT);
 	}
 	if (cmd->fd_in != -1)
 		execve(cmd->cmd_path, cmd->argv, cmd->infos->envp);
