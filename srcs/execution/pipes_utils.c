bbc64a0e885e82285399c42b1017d69ec59aafad
William DEBOTTE <wdebotte@student.42.fr>
2022-07-22 12:55:26 +0200

    Minor fixs

--- srcs/execution/pipes_utils.c
+++ srcs/execution/pipes_utils.c
@@ -6,7 +6,7 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:42:36 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/07/15 14:35:37 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/07/22 12:52:36 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,8 @@ void	close_pipes(char *error, int **pfds, int idx_pipes, int idx_tab)
 	i = -1;
 	while (++i < idx_pipes)
 	{
-		if (close(pfds[i][0]) < 0)
-			ft_putstr_fd(SH_NAME": close_pipes: close error\n", 2);
-		if (close(pfds[i][1]) < 0)
-			ft_putstr_fd(SH_NAME": close_pipes: close error\n", 2);
+		close(pfds[i][0]);
+		close(pfds[i][1]);
 	}
 	if (idx_tab > 0)
 	{
@@ -59,7 +57,8 @@ int	**init_pipefds(int npipes)
 		}
 		if (pipe(pfds[i]) < 0)
 		{
-			close_pipes(SH_NAME": pipefds: pipe error\n", pfds, i - 1, i);
+			free(pfds[i]);
+			close_pipes(SH_NAME": pipefds: pipe error\n", pfds, i, i);
 			return (NULL);
 		}
 	}
