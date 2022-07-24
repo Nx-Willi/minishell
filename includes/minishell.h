6883ada39f81e3849768ea7a94778a13415b3d05
XavierLB <xavier.lebaux@gmail.com>
2022-07-24 16:11:12 +0200

    Change heredog manengment

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 09:45:25 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/07/23 16:59:48 by xle-baux         ###   ########.fr       */
+/*   Updated: 2022/07/24 14:09:40 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,7 @@ char	*get_command_path(t_infos *infos, char *cmd);
 t_token	*get_tokens(char *input);
 t_token	*ignore_white_space(t_token *token);
 t_token	*redir(t_token *token, t_cmd *cmd);
-t_token	*get_heredoc(t_token *token);
+t_token	*get_heredoc(t_token *token, t_cmd *cmd);
 t_token	*add_token(void);
 t_token	*exit_status_check(t_token *token);
 
